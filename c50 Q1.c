2b1c3c82aa802e26f9f95e3467f11b28aadc4386
chunriyeqiongsaigao <92615097+chunriyeqiongsaigao@users.noreply.github.com>
2021-10-19 20:05:17 +0800

    试着推一下仓库

--- c50 Q1.c
+++ c50 Q1.c
@@ -1,6 +1,35 @@
 #include<stdio.h>
 int main(void)
-{  printf("*****");
-   pprintf(" *** ");
+{  int a,b,c;
+ 
+    puts("请输入三个能构成三角形的实数：");
 
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+
+    if(a==b)
+    {
+        if(a!=c)
+        {
+            printf("2");
+        }
+        else
+        {
+            printf("1");
+        }
+        
+    }
+    else
+    {
+        if(b==c)
+        {
+            printf("2");
+        }
+        else
+        {
+            printf("3");3
+        }
+    }
 }
